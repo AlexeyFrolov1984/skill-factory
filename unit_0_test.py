84b6b89fe49a73f574ef77db35ae0a72f8729dbe
AlexeyFrolov1984 <87722536+AlexeyFrolov1984@users.noreply.github.com>
2021-08-05 19:44:49 +0300

    Add files via upload

--- unit_0_test.py
+++ unit_0_test.py
@@ -4,22 +4,22 @@ box = [
     [1, ' ', ' ', ' '],
     [2, ' ', ' ', ' '],
 ]
-count = 0
-rez = ''
 
 
 def out_box():
+    """Функция вывода игрового поля"""
     for i in range(4):
         for j in range(4):
             print(box[i][j], end=" ")
         print()
 
 
-# Функция проверки выигрыша
 def check():
+    """Функция проверяет кто победил и возращает значение о победителе или ничьей"""
     win = ''
     line = ''
     for i in range(1, 4):
+        # Проверка выигрыша по горизонтали
         for j in range(1, 4):
             line += box[i][j]
         if line == 'XXX':
@@ -28,6 +28,7 @@ def check():
             win = 'НОЛИКИ ПОБЕДИЛИ'
         line = ''
     for j in range(1, 4):
+        # Проверка выигрыша по вертикали
         for i in range(1, 4):
             line += box[i][j]
         if line == 'XXX':
@@ -35,6 +36,7 @@ def check():
         if line == '000':
             win = 'НОЛИКИ ПОБЕДИЛИ'
         line = ''
+        # Проверка выигрыша по диагонали
     if box[1][1] == box[2][2] == box[3][3] == 'X' or box[3][1] == box[2][2] == box[1][3] == 'X':
         win = 'КРЕСТИКИ ПОБЕДИЛИ'
     if box[1][1] == box[2][2] == box[3][3] == '0' or box[3][1] == box[2][2] == box[1][3] == '0':
@@ -42,39 +44,55 @@ def check():
     return win
 
 
-out_box()
-while not rez and count != 9:
-    # Ввод координат X и проверка
-    coordinate = input("Введите координаты Х через пробел (х у)")
-    coordinate_x = int(coordinate[2]) + 1
-    coordinate_y = int(coordinate[0]) + 1
-    while box[coordinate_x][coordinate_y] != ' ':
-        print('Не тупи. Клетка занята. Введи еще раз!')
-        coordinate = input("Введите координаты Х через пробел (х у)")
-        coordinate_x = int(coordinate[2]) + 1
-        coordinate_y = int(coordinate[0]) + 1
-    box[coordinate_x][coordinate_y] = 'X'
-    out_box()
-    count += 1
-    check()
-    rez = check()
-    # Ввод координат 0 и проверка
-    if not rez and count != 9:
-        coordinate = input("Введите координаты 0 через пробел (х у)")
-        coordinate_x = int(coordinate[2]) + 1
-        coordinate_y = int(coordinate[0]) + 1
-
-        while box[coordinate_x][coordinate_y] != " ":
+def play_game():
+    """Функция игры - поочередный ввод крестиков и ноликов с проверкой на правильность ввода"""
+    rez = ''
+    count = 0
+    while not rez and count != 9:
+        # Ввод координат X и проверка
+        coord = input("Введите координаты Х через пробел (х -столбец,  у - строка)")
+        while len(coord) != 3 or coord[0] not in '012' or coord[2] not in '012' or coord[1] != ' ':
+            print("Что-то ввели не правильно!")
+            coord = input("Введите координаты Х через пробел (х -столбец,  у - строка)")
+        coord_x = int(coord[2]) + 1
+        coord_y = int(coord[0]) + 1
+        while box[coord_x][coord_y] != ' ':
             print('Не тупи. Клетка занята. Введи еще раз!')
-            coordinate = input("Введите координаты 0 через пробел (х у)")
-            coordinate_x = int(coordinate[2]) + 1
-            coordinate_y = int(coordinate[0]) + 1
-        box[coordinate_x][coordinate_y] = '0'
+            coord = input("Введите координаты Х через пробел (х -столбец,  у - строка)")
+            coord_x = int(coord[2]) + 1
+            coord_y = int(coord[0]) + 1
+        box[coord_x][coord_y] = 'X'
         out_box()
         count += 1
-        check()
         rez = check()
-if not rez:
-    print('НИЧЬЯ')
-else:
-    print(rez)
+        if not rez and count != 9:
+            # Ввод координат 0 и проверка
+            coord = input("Введите координаты 0 через пробел (х -столбец,  у - строка)")
+            while len(coord) != 3 or coord[0] not in '012' or coord[2] not in '012' or coord[1] != ' ':
+                print("Что-то ввели не правильно!")
+                coord = input("Введите координаты 0 через пробел (х -столбец,  у - строка)")
+            coord_x = int(coord[2]) + 1
+            coord_y = int(coord[0]) + 1
+            while box[coord_x][coord_y] != " ":
+                print('Не тупи. Клетка занята. Введи еще раз!')
+                coord = input("Введите координаты 0 через пробел (х -столбец,  у - строка)")
+                coord_x = int(coord[2]) + 1
+                coord_y = int(coord[0]) + 1
+            box[coord_x][coord_y] = '0'
+            # Вывод игрового поля после каждого хода
+        out_box()
+        count += 1
+        # Вызов функции проверки победителя
+        rez = check()
+    # Вывод результата игры на экран
+    if not rez:
+        print('НИЧЬЯ')
+    else:
+        print(rez)
+
+
+out_box()
+print('''Добро пожаловать в игру крестики нолики
+    Первый игрок ставит крестик на поле, второй нолик путем ввода координат
+    Выигрывает игрок, который соберет 3 подряд фигуры по вертикали, горизонтали или диагонали. ПОГНАЛИ!!!''')
+play_game()
